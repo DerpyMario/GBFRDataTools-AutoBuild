4d9db2cc6a7c708447a6e37b0744a4e30dd031f0
Nenkai <Nenkai@users.noreply.github.com>
2026-07-13 03:42:06 +0200

    entities: add game support attribute for entity properties

---
+++ GBFRDataTools.Entities/GameSupportAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBFRDataTools.Entities;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field)]
+public class GameSupportAttribute : Attribute
+{
+    public string Text { get; set; }
+
+    public GameSupportAttribute(string version)
+    {
+        Text = version;
+    }
+}
---
+++ GBFRDataTools.Entities/GameVersion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBFRDataTools.Entities;
+
+public class GameVersion
+{
+    public const string EndlessRagnarok = "2.0.0 ER";
+}
